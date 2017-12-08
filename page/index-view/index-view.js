be2eb24e9e48920b672aeb7d32be5639c982d561
zfmai <waltcow@gmail.com>
2017-12-08 16:50:40 +0800

    use widthFix mode to load images

--- page/index-view/index-view.js
+++ page/index-view/index-view.js
@@ -7,34 +7,8 @@ Page({
         comments: comments,
         product: product,
         images: images,
-        dp: 1,
         titleTop: '130px',
-        imageStyleData: {}, //图片样式数据
         countDownText: ''
-    },
-    //图片加载事件:全屏宽度750rpx减去padding值80rpx;计算出比率得到高度,用图片的URL作为key来设置data
-    imageLoad: function(e){
-        let imageStyle, imageStyleData,
-            id = e.currentTarget.dataset.id,
-            index = e.currentTarget.dataset.index,
-            img_w = e.detail.width,
-            img_h = e.detail.height,
-            ratio = 750/img_w;
-
-        if ((img_w/this.dp)>=750){
-            imageStyle = 'display: block; width: '+750+'rpx; height:'+ img_h*ratio +'rpx;';
-            if (index === 0) {
-                this.setData({ titleTop: img_h * 0.3 + 'rpx' })
-            }
-        } else {
-            imageStyle = 'display: block; width: '+img_w+'px; height:'+ img_h +'px;';
-            if (index === 0) {
-                this.setData({ titleTop: img_h * 0.3 + 'px' })
-            }
-        }
-        imageStyleData = this.data.imageStyleData;
-        imageStyleData[id] = imageStyle;
-        this.setData({ imageStyleData: imageStyleData });
     },
 
     showCountDown: function (year, month, day) {
@@ -87,7 +61,6 @@ Page({
 
     //获取图片
     onLoad: function () {
-        let self = this;
         let winWidth = null;
         let dp = 1;
         try {
@@ -96,13 +69,18 @@ Page({
         if (winWidth){
             dp = winWidth/750;
         }
-        this.dp = dp;
-        var interval = 1000;
+        this.setData({
+            titleTop: dp * 320 + 'px'
+        })
+
         var myDate = new Date();
+        var self = this;
         var y = myDate.getFullYear();
         var m = myDate.getMonth(); //获取当前月份(0-11,0代表1月)
         var d = myDate.getDate(); //获取当前日(1-31)
-        setInterval(() => this.showCountDown(y, m, d), interval);
-        citySelect.init(self)
+        setInterval(function () {
+            self.showCountDown(y, m, d)
+        }, 500)
+        citySelect.init(self);
     }
 })
